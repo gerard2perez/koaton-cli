7d1a378157d34c4feaca840589d40001528a4d1a
Gerardo Pérez <gerard2perez@outlook.com>
2016-11-10 07:45:12 -0600

    build($release): Make it commitizen friendly

---
+++ reloadcoverage.js
@@ -0,0 +1,27 @@
+const chokidar = require('chokidar');
+const livereload = require('gulp-livereload');
+const fs = require('graceful-fs');
+const glob = require('glob');
+function test() {
+	let script = '<script src="http://localhost:62627/livereload.js?snipver=1"></script>\n</body>';
+	glob.sync('testcoverage/**/*.html').forEach((file) => {
+		let index = fs.readFileSync(file, 'utf-8').replace('</body>', script);
+		fs.writeFileSync(file, index, 'utf-8');
+	});
+	livereload.reload();
+}
+var sourcewatcher = chokidar.watch(['testcoverage/lcov.info'], {
+	persistent: true,
+	ignoreInitial: true,
+	alwaysStat: false,
+	awaitWriteFinish: {
+		stabilityThreshold: 200,
+		pollInterval: 90
+	}
+});
+sourcewatcher.on('add', () => test()).on('change', () => test());
+livereload.listen({
+	port: 62627,
+	quiet: true
+});
+test();

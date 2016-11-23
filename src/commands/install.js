9f9af2c35c5de317639d16e5ed1d2204999bfb98
Gerardo Pérez <gerard2perez@outlook.com>
2016-11-23 14:29:48 -0600

    test: remove nginx from install command

--- src/commands/install.js
+++ src/commands/install.js
@@ -1,13 +1,13 @@
 import 'colors';
 import utils from '../utils';
 import command from '../command';
-import {copyconf} from './nginx';
+// import {copyconf} from './nginx';
 
 export default (new command(__filename, "SetUps a recent clonned proyect. (root/Administrator permission needed to work with nginx)"))
 .Args()
 	.Options()
 	.Action(async function() {
-		await copyconf(`${scfg.name}.conf`);
+		// await copyconf(`${scfg.name}.conf`);
 		await utils.shell("Installing bower dependencies", ["bower", "install"], process.cwd());
 		await utils.shell("Installing npm dependencies", ["npm", "install", "--loglevel", "info"], process.cwd());
 	});

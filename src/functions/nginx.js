ea623d15a782b15ca5ffe73690a822249fd99326
Gerardo Pérez Pérez <gerard2perez@outlook.com>
2017-04-10 18:28:00 -0500

    ci: nginx

--- src/functions/nginx.js
+++ src/functions/nginx.js
@@ -3,7 +3,12 @@ import exec from '../utils/exec';
 let nginxpath;
 async function getnginxpath () {
 	if (nginxpath === undefined) {
-		let log = await exec('nginx -t');
+		let cmd = 'nginx -t';
+		/* istanbul ignore next */
+		if (process.env.TRAVIS) {
+			cmd += '-c `pwd`/etc/nginx.conf -p `pwd`/etc/';
+		}
+		let log = await exec(cmd);
 		log = log.stdout || log.stderr;
 		if (log === undefined) {
 			throw new Error('Err! are you sure nginx is running and well configured.'.red);

9aa80edb1c87bc73de31939d4a2ae1956f6a5637
Gerardo Pérez Pérez <gerard2perez@outlook.com>
2017-09-24 18:11:53 -0500

    fix: make seed compatible with koaton 2.0.5

--- src/commands/seed.js
+++ src/commands/seed.js
@@ -2,16 +2,12 @@ import 'colors';
 import * as fs from 'fs-extra';
 import Command from 'cmd-line/lib/Command';
 import utils from '../utils';
-import inflector from '../support/inflector';
 
 export default (new Command(__filename, 'Creates or run seed in your project.'))
-.Args('model')
+	.Args('model')
 	.Options(['-g', '--generate', 'Generete a seed file for the specified model.'])
 	.Action(async function (model, options) {
 		process.env.NODE_ENV = 'development';
-		const Koa = require(ProyPath('/node_modules/koa'));
-		let app = new Koa();
-		app.inflector = inflector;
 		fs.ensureDirSync(ProyPath('seeds'));
 		if (model && options.generate && scfg.database.models[model] && !utils.canAccess(ProyPath('seeds', `${model}.js`))) {
 			console.log('file need creation');
@@ -27,6 +23,6 @@ export default (new Command(__filename, 'Creates or run seed in your project.'))
 				model: `${JSON.stringify(skey)},${JSON.stringify(seed)}`
 			});
 		} else if (!options.generate) {
-			await require(ProyPath('node_modules', 'koaton', 'lib', 'orm')).initialize(app, true);
+			await require(ProyPath('node_modules', 'koaton', 'middleware', 'orm')).initializeORM(true);
 		}
 	});
